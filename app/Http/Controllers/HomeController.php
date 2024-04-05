958c9537127c5f7a02f03818a60c133ee8f81e63
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-04-05 23:45:49 +0700

    update login & home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,25 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    use AuthenticatesUsers;
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+    protected $redirectTo = RouteServiceProvider::DASHBOARD;
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    
     public function index()
     {
         return view('client.index');
