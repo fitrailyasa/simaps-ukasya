c0d853e66e778d189dc4043dacfdaa0a04a161d1
FadilRifqi <fadilataullahrifqi@gmail.com>
2024-06-05 15:23:34 +0700

    fix jadwal tanding

--- database/migrations/2021_06_11_092718_create_jadwal_tanding_table.php
+++ database/migrations/2021_06_11_092718_create_jadwal_tanding_table.php
@@ -20,8 +20,8 @@ class CreateJadwalTandingTable extends Migration
             $table->string('babak');
             $table->string('jenis_kemenangan')->default('angka');
             $table->string('tahap')->default('persiapan');
-            $table->foreignId('sudut_biru')->nullable();
-            $table->foreignId('sudut_merah')->nullable();
+            $table->foreignId('sudut_biru');
+            $table->foreignId('sudut_merah');
             $table->integer('skor_merah')->default(0);
             $table->integer('skor_biru')->default(0);
             $table->integer('next_sudut');
