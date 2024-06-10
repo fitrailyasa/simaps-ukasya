380af1b7f3d64d33775f0897988cbf4948d3bc74
FadilRifqi <fadilataullahrifqi@gmail.com>
2024-06-10 12:51:59 +0700

    update

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data @yield('title')</title>
+    <title>Admin @yield('title')</title>
     @vite('resources/js/app.js')
 
     <!-- Google Font: Source Sans Pro -->
