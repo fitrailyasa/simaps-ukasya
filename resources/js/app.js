1d6ef59bf6fb603a8b3180c3a17df6400c94b326
FadilRifqi <fadilataullahrifqi@gmail.com>
2024-06-23 22:44:23 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,2 @@
 import "./bootstrap";
-import "../sass/app.scss";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
