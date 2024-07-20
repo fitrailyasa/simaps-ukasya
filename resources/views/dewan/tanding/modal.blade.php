723abfbde423eb8c010fb4d92f74e802a0a7c606
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-07-21 01:59:39 +0700

    update

--- resources/views/dewan/tanding/modal.blade.php
+++ resources/views/dewan/tanding/modal.blade.php
@@ -1,53 +1,79 @@
 <!-- Modal -->
-<div wire:ignore.self class="modal" id="jatuhan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header bg-danger">
-        <h5 class="modal-title" id="staticBackdropLabel">Verifikasi Jatuhan Juri</h5>
-      </div>
-      <div class="modal-body">
-       @if ($verifikasi_jatuhan)   
-          @foreach ($juri as $jjuri)
-              <div class="juri">
-                {{$jjuri['name']}} : {{$verifikasi_jatuhan_data[$jjuri['name']]}}
-              </div>
-          @endforeach
-          <div class="date">
-            <p>dibuat saat: {{$created_at}}</p>
-          </div>
-       @endif
-      </div>
-      <div class="modal-footer">
-        <button wire:click='VerifikasiJatuhanTrigger' type="button" class="btn btn-secondary btn-verif" >Minta Verifikasi Jatuhan Juri</button>
-        <button wire:click='tutupVerifikasiJatuhan' type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
+<div wire:ignore.self class="modal" id="jatuhan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #ffeccf;">
+                <h5 class="modal-title" id="staticBackdropLabel">Verifikasi Jatuhan Juri</h5>
+            </div>
+            <div class="modal-body d-flex justify-content-center">
+                @if ($verifikasi_jatuhan)
+                    @foreach ($juri as $jjuri)
+                        <div class="px-3">
+                            <h3>{{ $jjuri['name'] }}</h3>
+                            @if ($verifikasi_jatuhan_data[$jjuri['name']] == 'biru')
+                                <h3 class="bg-primary text-white text-center p-2">
+                                    {{ $verifikasi_jatuhan_data[$jjuri['name']] }}</h3>
+                            @elseif($verifikasi_jatuhan_data[$jjuri['name']] == 'merah')
+                                <h3 class="bg-danger text-white text-center p-2">
+                                    {{ $verifikasi_jatuhan_data[$jjuri['name']] }}</h3>
+                            @elseif($verifikasi_jatuhan_data[$jjuri['name']] == 'invalid')
+                                <h3 class="bg-warning text-white text-center p-2">
+                                    {{ $verifikasi_jatuhan_data[$jjuri['name']] }}</h3>
+                            @endif
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+            <div class="d-flex justify-content-center pb-2">
+                <button wire:click='VerifikasiJatuhanTrigger' type="button"
+                    class="btn btn-secondary btn-verif">Verifikasi</button>
+            </div>
+            <div class="d-flex justify-content-center pb-2">
+                <button wire:click='tutupVerifikasiJatuhan' type="button" class="btn btn-secondary"
+                    data-bs-dismiss="modal">Keluar</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <!-- Modal -->
-<div  wire:ignore.self class="modal" id="pelanggaran" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header bg-danger">
-        <h5 class="modal-title" id="staticBackdropLabel">Verifikasi Pelanggaran Juri</h5>
-      </div>
-      <div class="modal-body">
-       @if ($verifikasi_pelanggaran)   
-          @foreach ($juri as $jjuri)
-              <div class="juri">
-                {{$jjuri['name']}} : {{$verifikasi_pelanggaran_data[$jjuri['name']]}}
-              </div>
-          @endforeach
-          <div class="date">
-            <p>dibuat saat: {{$created_at}}</p>
-          </div>
-       @endif
-      </div>
-      <div class="modal-footer">
-        <button wire:click='VerifikasiPelanggaranTrigger' type="button" class="btn btn-secondary btn-verif" >Minta Verifikasi Pelanggaran Juri</button>
-        <button wire:click='tutupVerifikasiPelanggaran' type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
+<div wire:ignore.self class="modal" id="pelanggaran" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #ffeccf;">
+                <h5 class="modal-title" id="staticBackdropLabel">Verifikasi Pelanggaran Juri</h5>
+            </div>
+            <div class="modal-body d-flex justify-content-center">
+                @if ($verifikasi_pelanggaran)
+                    @foreach ($juri as $jjuri)
+                        <div class="px-3">
+                            <h3>{{ $jjuri['name'] }}</h3>
+                            @if ($verifikasi_pelanggaran_data[$jjuri['name']] == 'biru')
+                                <h3 class="bg-primary text-white text-center p-2">
+                                    {{ $verifikasi_pelanggaran_data[$jjuri['name']] }}
+                                </h3>
+                            @elseif($verifikasi_pelanggaran_data[$jjuri['name']] == 'merah')
+                                <h3 class="bg-danger text-white text-center p-2">
+                                    {{ $verifikasi_pelanggaran_data[$jjuri['name']] }}</h3>
+                            @elseif($verifikasi_pelanggaran_data[$jjuri['name']] == 'invalid')
+                                <h3 class="bg-warning text-white text-center p-2">
+                                    {{ $verifikasi_pelanggaran_data[$jjuri['name']] }}
+                                </h3>
+                            @endif
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+            <div class="d-flex justify-content-center pb-2">
+                <button wire:click='VerifikasiPelanggaranTrigger' type="button"
+                    class="btn btn-secondary btn-verif">Verifikasi</button>
+            </div>
+            <div class="d-flex justify-content-center pb-2">
+                <button wire:click='tutupVerifikasiPelanggaran' type="button" class="btn btn-secondary"
+                    data-bs-dismiss="modal">Keluar</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
